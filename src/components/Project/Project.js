dcdda51bc9e54f9a167b279375545df66aaa03a5
Michał Gołda <michal.golda@hotmail.com>
2023-05-08 20:17:30 +0200

    feat: create Project component

---
+++ src/components/Project/Project.js
@@ -0,0 +1,58 @@
+import Button from "../Button/Button";
+import {
+  StyledName,
+  StyledProject,
+  StyledDescription,
+  StyledTechnologyStack,
+  StyledImage,
+  StyledHeader,
+  StyledTextContainer,
+  StyledImageContainer,
+  StyledLinkInProject,
+  StyledLinkInTextContainer,
+} from "./Project.styled";
+
+const TechnologyStack = ({ technologyStack }) => {
+  const elements = [];
+  const length = technologyStack.length;
+
+  for (let i = 0; i < length - 2; i++) {
+    elements.push(<p key={technologyStack[i]}>{technologyStack[i]}</p>);
+    elements.push(<p key={i}>/</p>);
+  }
+
+  elements.push(
+    <p key={technologyStack[length - 1]}>{technologyStack[length - 1]}</p>
+  );
+
+  return <StyledTechnologyStack>{elements}</StyledTechnologyStack>;
+};
+
+export default function Project({
+  name,
+  description,
+  imageSrc,
+  githubLink,
+  technologyStack,
+}) {
+  return (
+    <StyledProject>
+      <StyledTextContainer>
+        <StyledHeader>
+          <StyledName>{name}</StyledName>
+          <TechnologyStack technologyStack={technologyStack} />
+        </StyledHeader>
+        <StyledDescription>{description}</StyledDescription>
+        <StyledLinkInTextContainer href={githubLink}>
+          <Button variant="outlined">Github</Button>
+        </StyledLinkInTextContainer>
+      </StyledTextContainer>
+      <StyledImageContainer>
+        <StyledImage src={imageSrc} fill />
+      </StyledImageContainer>
+      <StyledLinkInProject href={githubLink}>
+        <Button variant="outlined">Github</Button>
+      </StyledLinkInProject>
+    </StyledProject>
+  );
+}
---
+++ src/components/Project/Project.styled.js
@@ -0,0 +1,83 @@
+import styled from "styled-components";
+import Image from "next/image";
+
+export const StyledProject = styled.div`
+  display: flex;
+  row-gap: 16px;
+  flex-direction: column;
+
+  @media (${({ theme }) => theme.breakpoints.md}) {
+    flex-direction: row;
+    column-gap: 16px;
+  }
+`;
+
+export const StyledHeader = styled.div`
+  display: flex;
+  row-gap: 8px;
+  flex-direction: column;
+`;
+
+export const StyledName = styled.h3`
+  font-weight: 900;
+  font-size: 24px;
+  color: ${({ theme }) => theme.secondary};
+
+  @media (${({ theme }) => theme.breakpoints.md}) {
+    font-size: 32px;
+  }
+`;
+
+export const StyledTechnologyStack = styled.div`
+  display: flex;
+  column-gap: 8px;
+  flex-wrap: wrap;
+  justify-content: left;
+`;
+
+export const StyledDescription = styled.p`
+  color: ${({ theme }) => theme.colors.secondary};
+`;
+
+export const StyledImage = styled(Image)`
+  height: auto !important;
+  border-radius: 2px;
+  position: inherit !important;
+`;
+
+export const StyledTextContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  row-gap: 16px;
+
+  @media (${({ theme }) => theme.breakpoints.md}) {
+    width: 100%;
+  }
+`;
+
+export const StyledImageContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  max-width: 512px;
+`;
+
+const StyledLink = styled.a`
+  text-decoration: none;
+`;
+
+export const StyledLinkInTextContainer = styled(StyledLink)`
+  display: none;
+
+  @media (${({ theme }) => theme.breakpoints.md}) {
+    display: inherit;
+  }
+`;
+
+export const StyledLinkInProject = styled(StyledLink)`
+  display: inherit;
+
+  @media (${({ theme }) => theme.breakpoints.md}) {
+    display: none;
+  }
+`;
