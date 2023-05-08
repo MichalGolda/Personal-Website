f96908c6ebe4333a0a922ef988c69922515a9dac
Michał Gołda <michal.golda@hotmail.com>
2023-05-08 20:17:00 +0200

    feat: create Hero component

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,29 @@
+import {
+  StyledHero,
+  StyledTitle,
+  StyledDescription,
+  StyledTextContainer,
+  StyledImage,
+  StyledImageContainer,
+} from "./Hero.styled";
+import Button from "../Button/Button";
+
+export default function Hero() {
+  return (
+    <StyledHero>
+      <StyledTextContainer>
+        <StyledTitle>fullstack developer</StyledTitle>
+        <StyledDescription>
+          Vivamus posuere velit in ullamcorper rhoncus. Aenean odio eros,
+          iaculis at tincidunt id, feugiat ac nisl. Pellentesque volutpat.
+        </StyledDescription>
+        <a href="#contact">
+          <Button variant="primary">Kontakt</Button>
+        </a>
+      </StyledTextContainer>
+      <StyledImageContainer>
+        <StyledImage src="hero-illustration.svg" fill />
+      </StyledImageContainer>
+    </StyledHero>
+  );
+}
---
+++ src/components/Hero/Hero.styled.js
@@ -0,0 +1,50 @@
+import styled from "styled-components";
+import Image from "next/image";
+
+export const StyledHero = styled.div`
+  display: flex;
+  row-gap: 16px;
+  justify-content: left;
+  flex-direction: column;
+
+  @media (${({ theme }) => theme.breakpoints.md}) {
+    flex-direction: row;
+    justify-content: space-between;
+  }
+`;
+
+export const StyledTitle = styled.h1`
+  font-size: 48px;
+  font-weight: 900;
+  line-height: 100.5%;
+  text-transform: uppercase;
+
+  @media (${({ theme }) => theme.breakpoints.md}) {
+    font-size: 64px;
+  }
+`;
+
+export const StyledDescription = styled.p`
+  font-size: 20px;
+  color: ${({ theme }) => theme.colors.secondary};
+`;
+
+export const StyledTextContainer = styled.div`
+  display: flex;
+  row-gap: 16px;
+  flex-direction: column;
+`;
+
+export const StyledImageContainer = styled.div`
+  max-width: 512px;
+  width: 100%;
+`;
+
+export const StyledImage = styled(Image)`
+  position: inherit !important;
+  display: none;
+
+  @media (${({ theme }) => theme.breakpoints.md}) {
+    display: inherit;
+  }
+`;
