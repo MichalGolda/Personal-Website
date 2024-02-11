2a756477e24b460677af1e5958f34a8441ca8604
= <=>
2024-02-11 16:54:50 +0100

    feat: set metadata and base font family

---
+++ src/app/layout.tsx
@@ -0,0 +1,42 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({
+  subsets: ["latin", "latin-ext"],
+  weight: ["300", "400", "500", "600", "700"],
+});
+
+export const metadata: Metadata = {
+  title: "Dedykowane strony internetowe i aplikacje webowe - Michał Gołda",
+  description:
+    "Witaj na mojej personalnej stronie internetowej! Jestem Michał, pasjonuje się programowaniem i tworzeniem stron internetowych. Znajdziesz tutaj moje portfolio z zrealizowanymi projektami, które świadczą o moich umiejętnościach i pasji. Jeśli potrzebujesz wsparcia w zakresie tworzenia stron internetowych, aplikacji webowych lub chcesz porozmawiać o współpracy, skontaktuj się ze mną.",
+  keywords:
+    "Full-stack developer, Portfolio, Tworzenie stron internetowych, Tworzenie aplikacji webowych, Optymalizacja stron internetowych, Programowanie, UX, UI",
+  authors: {
+    name: "Michał Gołda",
+    url: "https://github.com/michalgolda",
+  },
+  robots: {
+    index: true,
+    follow: true,
+  },
+  icons: {
+    icon: "/favicon.ico",
+  },
+  other: {
+    "google-site-verification": "iGm5ckXvDyitftCfUi6T0DLfXc20aaThtTi40OrxzTM",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pl">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
