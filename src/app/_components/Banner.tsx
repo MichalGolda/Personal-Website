b8768e8e20d0678a954bf9e89ae41c22390c5bf0
= <=>
2024-02-11 19:46:32 +0100

    feat: create Banner component

---
+++ src/app/_components/Banner.tsx
@@ -0,0 +1,24 @@
+import { SecondaryButton } from "./Button";
+
+export default function Banner() {
+  return (
+    <div className="bg-primary w-full py-16 px-8 flex flex-col gap-y-8 text-center items-center">
+      <h2 className="text-4xl font-bold text-white">
+        Masz pomysł na projekt? Skontaktuj się ze mną i stwórzmy razem coś
+        wyjątkowego
+      </h2>
+      <div className="flex flex-col gap-y-1">
+        <h4 className="text-2xl text-white font-semibold">
+          <span className="text-[#B6C2FF80] font-bold text-5xl px-2">”</span>
+          Początek jest najważniejszą częścią pracy
+        </h4>
+        <span className="text-white text-base text-secondary font-medium">
+          Platon
+        </span>
+      </div>
+      <SecondaryButton className="w-fit" type="button">
+        Skontaktuj się
+      </SecondaryButton>
+    </div>
+  );
+}
