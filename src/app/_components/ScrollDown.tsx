b39f60d8a06b9b8e660cca256e9d51a9a2ed6a53
= <=>
2024-02-11 20:10:59 +0100

    feat: add ScrollDown component

---
+++ src/app/_components/ScrollDown.tsx
@@ -0,0 +1,7 @@
+export default function ScrollDown() {
+  return (
+    <span className="text-body font-semibold uppercase before:content-[''] before:h-40 before:w-0.5 before:bg-primary flex before:block before:mr-4">
+      <span className="pt-4">Scroll</span>
+    </span>
+  );
+}
