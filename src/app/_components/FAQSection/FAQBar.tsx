ed2a4549db709f462d229abbe12bebde9ea3a8af
= <=>
2024-02-12 16:51:46 +0100

    feat: add show and hide logic for FAQBar component

--- src/app/_components/FAQSection/FAQBar.tsx
+++ src/app/_components/FAQSection/FAQBar.tsx
@@ -1,23 +1,44 @@
 import Image from "next/image";
+import { MouseEventHandler } from "react";
 
 export type FAQBarProps = {
   title: string;
   content: string;
   showContent?: boolean;
+  onClick: MouseEventHandler<HTMLImageElement>;
 };
 
 const FAQBar: React.FC<FAQBarProps> = ({
   title,
   content,
   showContent = false,
+  onClick,
 }) => {
   return (
     <div>
       <div className="w-full h-auto py-4 px-8 bg-lightGrey flex flex-row justify-between items-center">
         <h4 className="text-2xl font-bold text-secondary">{title}</h4>
-        <Image src="/plus.svg" alt="plus" width={24} height={24} />
+        {!showContent ? (
+          <Image
+            className="hover:cursor-pointer"
+            onClick={onClick}
+            src="/plus.svg"
+            alt="plus"
+            width={24}
+            height={24}
+          />
+        ) : (
+          <Image
+            className="hover:cursor-pointer"
+            onClick={onClick}
+            src="/minus.svg"
+            alt="minus"
+            width={24}
+            height={24}
+          />
+        )}
       </div>
-      {showContent && <p>{content}</p>}
+      {showContent && <p className="text-body mt-4">{content}</p>}
     </div>
   );
 };
--- src/app/_components/FAQSection/FAQStack.tsx
+++ src/app/_components/FAQSection/FAQStack.tsx
@@ -1,12 +1,49 @@
+"use client";
+
+import { useState } from "react";
 import FAQBar from "./FAQBar";
 
 export default function FAQStack() {
+  const FAQData = [
+    {
+      title: "Jak długo trwa realizacja projektu?a",
+      content: "asdf",
+    },
+    {
+      title: "Jak długo trwa realizacja projektu?b",
+      content: "asdf",
+    },
+    {
+      title: "Jak długo trwa realizacja projektu?c",
+      content: "asdf",
+    },
+  ];
+  const [currentVisibleFAQAnswer, setCurrentVisibleFAQAnswer] = useState<
+    string | null
+  >();
+
+  const handleClick = (title: string) => {
+    if (currentVisibleFAQAnswer) {
+      if (currentVisibleFAQAnswer === title) {
+        setCurrentVisibleFAQAnswer(null);
+      } else {
+        setCurrentVisibleFAQAnswer(title);
+      }
+    } else {
+      setCurrentVisibleFAQAnswer(title);
+    }
+  };
+
   return (
     <div className="flex flex-col gap-y-8 mt-16">
-      <FAQBar title="Jak długo trwa realizacja projektu?" content="asdf" />
-      <FAQBar title="Jak długo trwa realizacja projektu?" content="asdf" />
-      <FAQBar title="Jak długo trwa realizacja projektu?" content="asdf" />
-      <FAQBar title="Jak długo trwa realizacja projektu?" content="asdf" />
+      {FAQData.map(({ title, content }) => (
+        <FAQBar
+          title={title}
+          content={content}
+          showContent={currentVisibleFAQAnswer === title}
+          onClick={() => handleClick(title)}
+        />
+      ))}
     </div>
   );
 }
