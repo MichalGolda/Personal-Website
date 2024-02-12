db991dd2b6e9c500db40820a4fb2b7ac47f1ee52
= <=>
2024-02-12 18:24:27 +0100

    feat: fix alert position

--- src/app/_components/Alert/Alert.tsx
+++ src/app/_components/Alert/Alert.tsx
@@ -6,7 +6,7 @@ export type AlertProps = {
 const Alert: React.FC<AlertProps> = ({ className, message }) => {
   return (
     <div
-      className={`w-full p-4 text-white top-0 font-bold text-center fixed z-[999]  ${className}`}
+      className={`w-full p-4 text-white top-0 left-0 font-bold text-center fixed z-[999]  ${className}`}
     >
       <p>{message}</p>
     </div>
