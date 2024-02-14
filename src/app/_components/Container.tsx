34127076cbab84240efdeb238cdb453e9858f927
= <=>
2024-02-14 13:11:14 +0100

    feat: set default value of className prop

--- src/app/_components/Container.tsx
+++ src/app/_components/Container.tsx
@@ -2,7 +2,7 @@ export type ContainerProps = React.PropsWithChildren<{
   className?: string;
 }>;
 
-const Container: React.FC<ContainerProps> = ({ className, children }) => {
+const Container: React.FC<ContainerProps> = ({ className = "", children }) => {
   return (
     <div
       className={`md:max-w-[1024px] lg:max-w-[1320px] w-full mx-auto md:px-16 px-4 ${className}`}
