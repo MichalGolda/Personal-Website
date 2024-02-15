5828c0ddbbb06f63f7869be90e95ddfa32149978
= <=>
2024-02-15 10:10:55 +0100

    feat: add sticky nav

--- src/app/_components/Header/Nav.tsx
+++ src/app/_components/Header/Nav.tsx
@@ -1,7 +1,9 @@
 "use client";
 
+import { useState, useEffect } from "react";
 import Image from "next/image";
 import MenuItem from "./MenuItem";
+import { Container } from "@/app/_components";
 
 export default function Nav() {
   const menuItems = [
@@ -26,19 +28,36 @@ export default function Nav() {
       href: "#contact",
     },
   ];
+  const [stickyNav, setStickyNav] = useState<boolean>(false);
+
+  useEffect(() => {
+    window.onscroll = () => setStickyNav(window.scrollY > 128);
+  }, []);
 
   return (
-    <nav className="flex flex-row justify-between pt-8 items-center">
-      <Image src="/logo.svg" alt="logo" width={151} height={29} />
-      <div className="flex flex-col gap-y-2 cursor-pointer lg:hidden">
-        <div className="block w-8 bg-secondary h-0.5"></div>
-        <div className="block w-8 bg-secondary h-0.5"></div>
-      </div>
-      <ul className="lg:flex lg:flex-row lg:gap-x-8 lg:list-none hidden">
-        {menuItems.map(({ name, href }) => {
-          return <MenuItem name={name} href={href} />;
-        })}
-      </ul>
+    <nav
+      className={`${
+        stickyNav
+          ? "fixed left-0 top-0 w-full bg-white border-b border-b-solid  border-b-lightGrey z-[999]"
+          : ""
+      }`}
+    >
+      <Container
+        className={`flex flex-row justify-between pt-4 pb-4 items-center ${
+          stickyNav ? "" : "!px-0"
+        }`}
+      >
+        <Image src="/logo.svg" alt="logo" width={151} height={29} />
+        <div className="flex flex-col gap-y-2 cursor-pointer lg:hidden">
+          <div className="block w-8 bg-secondary h-0.5"></div>
+          <div className="block w-8 bg-secondary h-0.5"></div>
+        </div>
+        <ul className="lg:flex lg:flex-row lg:gap-x-8 lg:list-none hidden">
+          {menuItems.map(({ name, href }) => {
+            return <MenuItem name={name} href={href} />;
+          })}
+        </ul>
+      </Container>
     </nav>
   );
 }
