d250cc22476e9b3b9ff968129641e30bb368af79
= <=>
2024-02-19 18:44:55 +0100

    refactor: remove useless console.log

--- src/app/_context/inViewSectionContext.tsx
+++ src/app/_context/inViewSectionContext.tsx
@@ -31,8 +31,6 @@ export const useInViewSection = (id: string) => {
   const { setSection } = useInViewSectionContext();
 
   useEffect(() => {
-    console.log(id, inView);
-
     inView && setSection(id);
   }, [inView]);
 
