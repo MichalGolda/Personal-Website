05983fb40755bcd30282afdce4ddfe051adc95e6
= <=>
2024-02-19 18:41:42 +0100

    feat: hide scroll down on sm devices

--- src/app/_components/ScrollDown.tsx
+++ src/app/_components/ScrollDown.tsx
@@ -1,6 +1,6 @@
 export default function ScrollDown() {
   return (
-    <span className="-mt-16 text-body font-semibold uppercase before:content-[''] before:h-40 before:w-0.5 before:bg-primary flex before:block before:mr-4">
+    <span className="-mt-16 hidden md:flex text-body font-semibold uppercase before:content-[''] before:h-40 before:w-0.5 before:bg-primary before:block before:mr-4">
       <span className="pt-4">Scroll</span>
     </span>
   );
