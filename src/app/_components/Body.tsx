e44fbe08f9482a6a709027255e712247866441cb
= <=>
2024-02-20 12:35:54 +0100

    feat: add google analytics script

--- src/app/_components/Body.tsx
+++ src/app/_components/Body.tsx
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import InViewSectionContextProvider from "../_context/inViewSectionContext";
 import { useNavContext } from "../_context/navContext";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const inter = Inter({
   subsets: ["latin", "latin-ext"],
@@ -17,10 +18,9 @@ export default function Body({ children }: React.PropsWithChildren) {
     <body
       className={`${inter.className} ${hamburger ? "overflow-hidden" : ""}`}
     >
-      <InViewSectionContextProvider>
-        {children}
-        <SpeedInsights />
-      </InViewSectionContextProvider>
+      <InViewSectionContextProvider>{children}</InViewSectionContextProvider>
+      <SpeedInsights />
+      <GoogleAnalytics gaId="G-5LGQ62V141" />
     </body>
   );
 }
