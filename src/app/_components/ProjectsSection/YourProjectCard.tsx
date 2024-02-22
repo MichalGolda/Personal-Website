fb75a8ca9d4ce9f544c8431080d9fbd95efa5358
= <=>
2024-02-22 18:16:07 +0100

    feat: add duration

--- src/app/_components/ProjectsSection/YourProjectCard.tsx
+++ src/app/_components/ProjectsSection/YourProjectCard.tsx
@@ -5,6 +5,7 @@ export default function YourProjectCard() {
     <div
       className="w-full h-full bg-secondary flex items-center justify-center"
       data-aos="zoom-in"
+      data-aos-duration="500"
     >
       <div className="flex flex-col gap-y-4 items-center">
         <h4 className="text-white text-lg md:text-xl lg:text-2xl font-bold">
