b798123281e955d5995c686f347a9de8604ce0f4
= <=>
2024-02-22 13:19:40 +0100

    feat: update TechnologiesSection component
    add icons
    add content

--- src/app/_components/TechnologiesSection/TechnologyCard.tsx
+++ src/app/_components/TechnologiesSection/TechnologyCard.tsx
@@ -1,15 +1,40 @@
-export type TechnologyCardProps = {
-  title: string;
-  content: string;
+import Image from "next/image";
+
+type TechnologyIcon = {
+  src: string;
+  alt: string;
 };
 
-const TechnologyCard: React.FC<TechnologyCardProps> = ({ title, content }) => {
+export type TechnologyCardProps = {
+  title: string;
+  content: () => React.ReactElement;
+  icons?: TechnologyIcon[];
+};
+
+const TechnologyCard: React.FC<TechnologyCardProps> = ({
+  title,
+  content,
+  icons,
+}) => {
   return (
     <div className="w-full flex flex-col gap-y-4">
       <h4 className="text-lg md:text-xl lg:text-2xl text-secondary font-bold">
         {title}
       </h4>
-      <p className="text-sm md:text-base text-body">{content}</p>
+      <p className="text-sm md:text-base text-body">{content()}</p>
+      {icons && (
+        <div className="flex flex-row flex-wrap gap-4">
+          {icons.map(({ src, alt }) => (
+            <Image
+              className="max-w-8 max-h-8"
+              src={src}
+              alt={alt}
+              width={32}
+              height={32}
+            />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
