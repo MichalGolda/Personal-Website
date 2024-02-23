a45f926d736dc117aa8628a91336ac5247ea400d
= <=>
2024-02-23 15:13:10 +0100

    feat: add cookie icon

--- src/app/_components/CookieConsent.tsx
+++ src/app/_components/CookieConsent.tsx
@@ -1,7 +1,8 @@
 "use client";
 
+import Image from "next/image";
 import { PrimaryButton } from "@/app/_components";
-import { useEffect, useTransition } from "react";
+import { useTransition } from "react";
 import acceptCookieConsent from "../_actions/cookieConsent/cookieConsent";
 
 export type CookieConsentProps = {
@@ -9,25 +10,30 @@ export type CookieConsentProps = {
 };
 
 export default function CookieConsent({ hidden = true }: CookieConsentProps) {
-  const [isPending, startTransition] = useTransition();
-
-  useEffect(() => {
-    console.log(isPending);
-  }, [isPending]);
+  const [_, startTransition] = useTransition();
 
   return (
     <div
-      className={`duration-500 fixed bottom-0 w-full sm:max-w-96 transition-transform md:max-w-96 left-0 sm:ml-4 sm:mb-4 bg-secondary p-4 ${
+      className={`duration-500 fixed bottom-0 w-full sm:max-w-md transition-transform left-0 sm:ml-4 sm:mb-4 bg-secondary p-8 ${
         hidden ? "translate-x-[-200%]" : "translate-x-[0]"
       }`}
     >
-      <p className="text-sm md:text-base text-white font-medium">
-        Strona korzysta z ciasteczek, aby zapewnić najlepszą jakość korzystania
-        z mojej witryny.
-      </p>
+      <div className="flex flex-row items-center gap-x-4">
+        <Image
+          src="/cookie.svg"
+          className="h-10 w-10 md:w-16 md:h-16"
+          alt="cookie"
+          width={64}
+          height={64}
+        />
+        <p className="text-sm md:text-base text-white font-medium">
+          Strona korzysta z ciasteczek, aby zapewnić najlepszą jakość
+          korzystania z witryny.
+        </p>
+      </div>
       <PrimaryButton
         onClick={() => startTransition(async () => await acceptCookieConsent())}
-        className="!py-2 mt-4"
+        className="!py-2 mt-4 w-full"
       >
         Akcpetuję
       </PrimaryButton>
