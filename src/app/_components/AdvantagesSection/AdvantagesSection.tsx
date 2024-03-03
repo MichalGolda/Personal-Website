0160f7cc1078d08465f8ef913ccb87bdd2dc18d9
= <=>
2024-03-03 19:16:34 +0100

    fix: AdvantageSection has some responsive issues

--- src/app/_components/AdvantagesSection/AdvantagesSection.tsx
+++ src/app/_components/AdvantagesSection/AdvantagesSection.tsx
@@ -11,14 +11,7 @@ export default function AdvantagesSection() {
     <section ref={ref} id="advantages" className="my-64">
       <Container>
         <SectionHeading headline="zalety" title="Dlaczego warto mi zaufać?" />
-        <div className="my-16 lg:flex lg:flex-row gap-16 flex flex-col gap-y-16 md:grid md:grid-cols-2 items-start">
-          <AdvantageCard
-            title="Rozwiązania dopasowane do oczekiwań klienta"
-            content="Każdy projekt rozpoczynam od zebrania szczegółowych informacji na temat tego jak ma wyglądać końcowe rozwiązanie. W trakcie realizacji konsultuje postępy po to żeby jak najlepiej wpasować się w twoje oczekiwania."
-            iconSrc="/measure.svg"
-            data-aos="fade-right"
-            data-aos-duration="500"
-          />
+        <div className="my-16 gap-16 lg:grid-cols-3 flex flex-col gap-y-16 md:grid md:grid-cols-2 items-start">
           <AdvantageCard
             title="Przejrzyste rozliczenia"
             content="Przed rozpoczęciem projektu dokonuje wyceny w której zawarte są wszystkie wykonywane usługi. Dzięki, temu wiesz za co płacisz."
@@ -26,7 +19,14 @@ export default function AdvantagesSection() {
             data-aos="fade-up"
             data-aos-duration="1000"
           />
-          <div className="col-span-2">
+          <AdvantageCard
+            title="Rozwiązania dopasowane do oczekiwań klienta"
+            content="Każdy projekt rozpoczynam od zebrania szczegółowych informacji na temat tego jak ma wyglądać końcowe rozwiązanie. W trakcie realizacji konsultuje postępy po to żeby jak najlepiej wpasować się w twoje oczekiwania."
+            iconSrc="/measure.svg"
+            data-aos="fade-right"
+            data-aos-duration="500"
+          />
+          <div className="col-span-2 lg:col-span-1">
             <AdvantageCard
               title="Realizacja w określonym terminie"
               content="Szczegółowo planuje etapy pracy dzięki czemu każde rozwiązanie jest gotowe na czas."
