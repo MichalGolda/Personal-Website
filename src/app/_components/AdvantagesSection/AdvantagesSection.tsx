3e08174a5dbb1e9d61b6302327a529ae603de493
= <=>
2024-03-03 19:08:07 +0100

    feat: add AdvantagesSection content

--- src/app/_components/AdvantagesSection/AdvantagesSection.tsx
+++ src/app/_components/AdvantagesSection/AdvantagesSection.tsx
@@ -14,14 +14,14 @@ export default function AdvantagesSection() {
         <div className="my-16 lg:flex lg:flex-row gap-16 flex flex-col gap-y-16 md:grid md:grid-cols-2 items-start">
           <AdvantageCard
             title="Rozwiązania dopasowane do oczekiwań klienta"
-            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec hendrerit justo sit amet orci interdum, facilisis commodo elit gravida. Suspendisse potenti. Cras non erat et magna aliquam convallis vel ac dolor."
+            content="Każdy projekt rozpoczynam od zebrania szczegółowych informacji na temat tego jak ma wyglądać końcowe rozwiązanie. W trakcie realizacji konsultuje postępy po to żeby jak najlepiej wpasować się w twoje oczekiwania."
             iconSrc="/measure.svg"
             data-aos="fade-right"
             data-aos-duration="500"
           />
           <AdvantageCard
             title="Przejrzyste rozliczenia"
-            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec hendrerit justo sit amet orci interdum, facilisis commodo elit gravida. Suspendisse potenti. Cras non erat et magna aliquam convallis vel ac dolor."
+            content="Przed rozpoczęciem projektu dokonuje wyceny w której zawarte są wszystkie wykonywane usługi. Dzięki, temu wiesz za co płacisz."
             iconSrc="/dollar.svg"
             data-aos="fade-up"
             data-aos-duration="1000"
@@ -29,7 +29,7 @@ export default function AdvantagesSection() {
           <div className="col-span-2">
             <AdvantageCard
               title="Realizacja w określonym terminie"
-              content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec hendrerit justo sit amet orci interdum, facilisis commodo elit gravida. Suspendisse potenti. Cras non erat et magna aliquam convallis vel ac dolor."
+              content="Szczegółowo planuje etapy pracy dzięki czemu każde rozwiązanie jest gotowe na czas."
               iconSrc="/calendar.svg"
               data-aos="fade-left"
               data-aos-duration="1500"
