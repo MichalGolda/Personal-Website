a1e2aa02e050820c19c24d09921bb9b0b203b084
michalgolda <michal.golda@hotmail.com>
2024-05-31 14:10:15 +0200

    chore: add mapping for src directory

--- jest.config.ts
+++ jest.config.ts
@@ -3,11 +3,15 @@ import nextJest from 'next/jest.js'
  
 const createJestConfig = nextJest({
   dir: './',
+
 })
  
 const config: Config = {
   coverageProvider: 'v8',
   testEnvironment: 'jsdom',
- }
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
+}
  
 export default createJestConfig(config)
