4fe0948d4f097513d13ef2858896067eac664225
michalgolda <michal.golda@hotmail.com>
2024-05-31 14:55:28 +0200

    test: just use link reference

--- src/app/_components/Header/__tests__/Hero.test.tsx
+++ src/app/_components/Header/__tests__/Hero.test.tsx
@@ -10,9 +10,7 @@ describe("Hero component", () => {
         const subtitle = screen.getByTestId("subtitle")
         const links = screen.getAllByRole("link")
         const contactLink = links[0]
-        const contactButton = contactLink.children[0]
         const portfolioLink = links[1]
-        const portfolioButton = portfolioLink.children[0]
 
         expect(title).toBeInTheDocument()
         expect(title.tagName).toBe("H1")
@@ -22,8 +20,7 @@ describe("Hero component", () => {
     
         expect(contactLink).toBeInTheDocument()
         expect(contactLink).toHaveAttribute("href", "#contact")
-
-        expect(contactButton.textContent).toBe("Stwórzmy coś razem")
+        expect(contactLink.textContent).toBe("Stwórzmy coś razem")
 
         expect(portfolioLink).toBeInTheDocument()
         expect(portfolioLink).toHaveAttribute("href", "#portfolio")
