7b9320bba632510ddd6bb715f82934cea3a53928
michalgolda <michal.golda@hotmail.com>
2024-06-03 21:01:50 +0200

    chore: remove useless import

--- src/app/_components/__tests__/Footer.test.tsx
+++ src/app/_components/__tests__/Footer.test.tsx
@@ -1,7 +1,6 @@
 import "@testing-library/jest-dom";
 import { render } from "@testing-library/react";
 import Footer from "../Footer";
-import { useCurrentYear } from "@/app/_hooks/useCurrentYear";
 
 describe("Footer component", () => {
   it("renders without errors", () => {
