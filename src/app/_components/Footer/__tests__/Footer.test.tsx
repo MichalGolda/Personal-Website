8bcb20d3136ccc8eb92eaea8c59b51af0b9ceb6d
michalgolda <michal.golda@hotmail.com>
2024-06-04 10:19:30 +0200

    test: ensure SocialIcon component renders without errors

--- src/app/_components/Footer/__tests__/Footer.test.tsx
+++ src/app/_components/Footer/__tests__/Footer.test.tsx
@@ -1,6 +1,6 @@
 import "@testing-library/jest-dom";
 import { render } from "@testing-library/react";
-import Footer from "../Footer/Footer";
+import Footer from "../Footer";
 
 describe("Footer component", () => {
   it("renders without errors", () => {
