53c6bac4c2ad2dbb4e89749052f76c6949d94b02
michalgolda <michal.golda@hotmail.com>
2024-06-04 12:24:38 +0200

    test: ensure ProjectExternalLink component renders without errors

---
+++ src/app/_components/ProjectsSection/__tests__/ProjectExternalLink.test.tsx
@@ -0,0 +1,35 @@
+import "@testing-library/jest-dom";
+import { render } from "@testing-library/react";
+import ProjectExternalLink from "../ProjectExternalLink";
+
+describe("ProjectExternalLink component", () => {
+  it("renders without errors", () => {
+    const { container } = render(
+      <ProjectExternalLink href="https://test.pl" name="test name" />
+    );
+
+    const link = container.firstChild;
+    const image = link?.lastChild;
+
+    expect(link).toBeInTheDocument();
+    expect(link).toHaveAttribute("href", "https://test.pl");
+    expect(link?.textContent).toBe("test name");
+
+    expect(image).toBeInTheDocument();
+    expect(image).toHaveAttribute(
+      "src",
+      "/_next/image?url=%2Farrow.webp&w=64&q=75"
+    );
+    expect(image).toHaveAttribute("alt", "arrow");
+    expect(image).toHaveAttribute("width", "32");
+    expect(image).toHaveAttribute("height", "0");
+  });
+
+  it("renders unchanged", () => {
+    const { container } = render(
+      <ProjectExternalLink href="https://test.pl" name="test name" />
+    );
+
+    expect(container).toMatchSnapshot();
+  });
+});
